sonyps5201314/Dependencies

commit 1e3f6115ffbdc240f21edcde8779cb2bf86466c2

Copy file path and show in file explorer implemented

DependenciesGui/DependencyWindow.xaml.cs
@@ -1346,6 +1346,28 @@ namespace Dependencies
             }
         }
 
+        private void CopyFilePath_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            ModuleTreeViewItem Source = e.Source as ModuleTreeViewItem;
+            String SelectedModuleName = Source.GetTreeNodeHeaderName(Dependencies.Properties.Settings.Default.FullPath);
+            if (Source == null)
+                return;
+
+            Clipboard.SetText(SelectedModuleName);
+        }
+
+        private void OpenInExplorer_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            ModuleTreeViewItem Source = e.Source as ModuleTreeViewItem;
+            if (Source == null)
+                return;
+
+            String SelectedModuleName = Source.GetTreeNodeHeaderName(Dependencies.Properties.Settings.Default.FullPath);
+            String commandParameter = "/select,\"" + SelectedModuleName + "\"";
+
+            Process.Start("explorer.exe", commandParameter);
+        }
+
         private void ExpandAllParentNode(ModuleTreeViewItem Item)
         {
             if (Item != null)

DependenciesGui/DependencyWindow.xaml
@@ -50,6 +50,8 @@
                     <RoutedUICommand x:Key="DoExpandAllNodes" Text="Expand All" />
                     <RoutedUICommand x:Key="DoCollapseAllNodes" Text="Collapse All" />
                     <RoutedUICommand x:Key="DoFindModuleInList" Text="Highlight Matching Module In List" />
+                    <RoutedUICommand x:Key="CopyFilePath" Text="Copy File Path" />
+                    <RoutedUICommand x:Key="OpenInExplorer" Text="Open in File Explorer" />
 
                     <ContextMenu x:Key="TreeItemContextMenu">
                         <MenuItem Header="Highlight Matching Module In List" 
@@ -59,10 +61,14 @@
                                 IsEnabled="True" />
                         <Separator Height="3" Margin="-1,0,0,0"/>
                         <MenuItem Header="Copy File Path" 
-                                Command="{Binding Path=DataContext.MoreInfo, RelativeSource={RelativeSource Mode=FindAncestor, AncestorType=ListView}}" 
+                                Command="{StaticResource CopyFilePath}"
                                 Background="WhiteSmoke" 
                                 InputGestureText="Ctrl+C" 
-                                IsEnabled="False"  />
+                                IsEnabled="True"  />
+                        <MenuItem Header="Open in File Explorer" 
+                                Command="{StaticResource OpenInExplorer}"
+                                Background="WhiteSmoke" 
+                                IsEnabled="True"  />
                         <Separator Height="3" Margin="-1,0,0,0"/>
                         <MenuItem Header="Expand All" 
                                 Command="{StaticResource DoExpandAllNodes}"
@@ -171,6 +177,8 @@
                     <CommandBinding Command="{StaticResource DoExpandAllNodes}" Executed="ExpandAllNodes_Executed"/>
                     <CommandBinding Command="{StaticResource DoCollapseAllNodes}" Executed="CollapseAllNodes_Executed"/>
                     <CommandBinding Command="{StaticResource DoFindModuleInList}" Executed="DoFindModuleInList_Executed"/>
+                    <CommandBinding Command="{StaticResource CopyFilePath}" Executed="CopyFilePath_Executed"/>
+                    <CommandBinding Command="{StaticResource OpenInExplorer}" Executed="OpenInExplorer_Executed"/>
                 </TreeView.CommandBindings>
             </TreeView>
             

commit 3330dcf335ea6c77466e02d8af16cd0ee1a01a6a

Fixed an issue where the "Open in File Explorer" function would fail to work if "FullPaths" was not checked.
This bug was introduced by 1e3f6115ffbdc240f21edcde8779cb2bf86466c2

DependenciesGui/DependencyWindow.xaml.cs
@@ -1362,8 +1362,7 @@ namespace Dependencies
             if (Source == null)
                 return;
 
-            String SelectedModuleName = Source.GetTreeNodeHeaderName(Dependencies.Properties.Settings.Default.FullPath);
-            String commandParameter = "/select,\"" + SelectedModuleName + "\"";
+            String commandParameter = "/select,\"" + Source.ModuleFilePath + "\"";
 
             Process.Start("explorer.exe", commandParameter);
         }